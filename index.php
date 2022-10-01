09439b7fe351b1b3331251cf924c9b9d7591e7f9
kamran <kamran2201f@aptechsite.net>
2022-10-01 20:20:09 +0500

    UsingPHP

---
+++ index.php
@@ -0,0 +1,251 @@
+<?php
+if(isset($_POST['Submit']))
+{
+    $fname =$_POST['fname'];
+    
+    $lname =$_POST['lname'];
+    $sname =$_POST['sname'];
+    $uname =$_POST['uname'];
+    $ename =$_POST['ename'];
+    $cname =$_POST['cname'];
+    echo $fname . "&nbsp &nbsp &nbsp" . $lname . "&nbsp &nbsp &nbsp" . $sname . "&nbsp &nbsp &nbsp" . $uname . "&nbsp &nbsp &nbsp" . $ename . "&nbsp &nbsp &nbsp" . $cname;
+    echo "&nbsp &nbsp &nbsp";
+
+ 
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+<form class="well form-horizontal" action=" " method="Post"  id="contact_form">
+<fieldset>
+<!-- Form Name -->
+<legend><center><h2><b>Registration Form</b></h2></center></legend><br>
+<!-- Text input-->
+<div class="form-group">
+<label class="col-md-4 control-label" >First Name</label>  
+<div class="col-md-4 inputGroupContainer">
+<div class="input-group">
+<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+<input   placeholder="First Name" name="fname" class="form-control"  type="text">
+</div>
+</div>
+</div>
+<!-- Text input-->
+<div class="form-group">
+<label class="col-md-4 control-label" >Last Name</label> 
+<div class="col-md-4 inputGroupContainer">
+<div class="input-group">
+<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+<input placeholder="Last Name" name="lname" class="form-control"  type="text">
+</div>
+</div>
+</div>
+<div class="form-group"> 
+<label class="col-md-4 control-label">Department / Office</label>
+<div class="col-md-4 selectContainer">
+<div class="input-group">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
+<select  class="form-control selectpicker" name="sname">
+  <option value="">Select your Department/Office</option>
+  <option>Department of Engineering</option>
+  <option>Department of Agriculture</option>
+  <option >Accounting Office</option>
+  <option >Tresurer's Office</option>
+  <option >MPDC</option>
+  <option >MCTC</option>
+  <option >MCR</option>
+  <option >Mayor's Office</option>
+  <option >Tourism Office</option>
+</select>
+</div>
+</div>
+</div>
+<!-- Text input-->
+<div class="form-group">
+<label class="col-md-4 control-label">Username</label>  
+<div class="col-md-4 inputGroupContainer">
+<div class="input-group">
+<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+<input placeholder="Username" class="form-control" name="uname" type="text">
+</div>
+</div>
+</div>
+<!-- Text input-->
+<div class="form-group">
+<label class="col-md-4 control-label" >Password</label> 
+<div class="col-md-4 inputGroupContainer">
+<div class="input-group">
+<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+<input name="user_password" placeholder="Password" class="form-control"  type="password">
+</div>
+</div>
+</div>
+<!-- Text input-->
+<div class="form-group">
+<label class="col-md-4 control-label" >Confirm Password</label> 
+<div class="col-md-4 inputGroupContainer">
+<div class="input-group">
+<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+<input name="confirm_password" placeholder="Confirm Password" class="form-control"  type="password">
+</div>
+</div>
+</div>
+<!-- Text input-->
+   <div class="form-group">
+<label class="col-md-4 control-label">E-Mail</label>  
+<div class="col-md-4 inputGroupContainer">
+<div class="input-group">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+<input  placeholder="E-Mail Address" class="form-control" name="ename" type="text">
+</div>
+</div>
+</div>
+<!-- Text input-->
+<div class="form-group">
+<label class="col-md-4 control-label">Contact No.</label>  
+<div class="col-md-4 inputGroupContainer">
+<div class="input-group">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+<input placeholder="(639)" class="form-control" name="cname" type="text">
+</div>
+</div>
+</div>
+<!-- Select Basic -->
+<!-- Success message -->
+<div class="alert alert-success" role="alert" id="success_message">Success <i class="glyphicon glyphicon-thumbs-up"></i> Success!.</div>
+<!-- Button -->
+<div class="form-group">
+<label class="col-md-4 control-label"></label>
+<div class="col-md-4"><br>
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button type="submit" class="btn btn-warning" name="Submit">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSUBMIT <span class="glyphicon glyphicon-send"></span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</button>
+</div>
+</div>
+</fieldset>
+</form>
+</div>
+<!-- </div>
+<script>  $(document).ready(function() {
+    $('#contact_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            first_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                    },
+                        notEmpty: {
+                        message: 'Please enter your First Name'
+                    }
+                }
+            },
+             last_name: {
+                validators: {
+                     stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Last Name'
+                    }
+                }
+            },
+			 user_name: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Username'
+                    }
+                }
+            },
+			 user_password: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Password'
+                    }
+                }
+            },
+			confirm_password: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please confirm your Password'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please enter your Email Address'
+                    },
+                    emailAddress: {
+                        message: 'Please enter a valid Email Address'
+                    }
+                }
+            },
+            contact_no: {
+                validators: {
+                  stringLength: {
+                        min: 12, 
+                        max: 12,
+                    notEmpty: {
+                        message: 'Please enter your Contact No.'
+                     }
+                }
+            },
+			 department: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please select your Department/Office'
+                    }
+                }
+            },
+                }
+            }
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+});</script>/.container -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
